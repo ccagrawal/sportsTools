3164e7f04b12b17950c1c46dc6b21ce2c8a83816
ccagrawal <ccagrawal@gmail.com>
2015-11-22 17:28:07 -0600

    Add functions to get specific player stats

---
+++ R/GetPlayerSpecificStats.R
@@ -0,0 +1,31 @@
+#' Player's stat in a season.
+#'
+#' @param player NBA player (e.g. "James Harden")
+#' @param year NBA season (e.g. 2008 for the 2007-08 season)
+#' @param stat Which stat do you want (e.g. "on-off")
+#' @return data frame with player's stats
+#' @keywords player
+#' @importFrom XML readHTMLTable
+#' @export
+#' @examples
+#' GetPlayerSpecificStat('Anthony Davis', 2015, 'on-off)
+
+GetPlayerSpecificStat <- function(player, year, stat) {
+  
+  options(stringsAsFactors = FALSE)
+  
+  player.ids <- GetPlayerIDs(year = year)
+  id <- player.ids[which(player.ids$name == player), 'id']
+  
+  base.url <- paste0("http://www.basketball-reference.com/players", id, "/")
+  
+  # On-off stat ratings
+  if (stat == 'on-off') {
+    url <- paste0(base.url, 'on-off/', year, '/')
+    
+    table <- readHTMLTable(url)[[1]]
+    table[, -c(1, 2)] <- sapply(table[, -c(1, 2)], as.numeric)
+    
+    return(readHTMLTable(url)[[1]])
+  }
+}
