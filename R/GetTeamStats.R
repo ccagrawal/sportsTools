ddcb59cac8c37654f0dfd356c77daac91d3f4e31
ccagrawal <ccagrawal@gmail.com>
2015-11-26 16:08:40 -0600

    Fix Team Stats function

--- R/GetTeamStats.R
+++ R/GetTeamStats.R
@@ -1,6 +1,7 @@
 #' Team Stats.
 #'
 #' @param year NBA season (e.g. 2008 for the 2007-08 season)
+#' @param stat.type 'regular' or 'advanced'
 #' @param season.type 'regular' or 'playoffs'
 #' @return data frame with wins and losses for that season
 #' @keywords team
@@ -22,16 +23,18 @@ GetTeamStats <- function(year, stat.type = 'regular', season.type = 'regular') {
   url <- gsub('YYYY', year, url)
   tables <- readHTMLTable(url)
   
-  # Get table and convert appropriate columns to numeric
+  # Get table, convert appropriate columns to numeric, remove asterisks from team names
   if (stat.type == 'advanced') {
     stats <- tables[['misc']]
     stats[, -c(2, 23, 24)] <- lapply(stats[, -c(2, 23, 24)], as.numeric)
     stats$Attendance <- as.numeric(gsub(',', '', stats$Attendance))
+    stats$Team <- gsub('\\*', '', stats$Team)
   } else {
     stats <- tables[['team']]
     stats[, -2] <- lapply(stats[, -2], as.numeric)
+    stats$Team <- gsub('\\*', '', stats$Team)
   }
   
-  stats <- stats[-which(is.na(stats$Rk)), ]   # Remove league average row
+  stats <- stats[-which(is.na(stats$Rk)), -1]   # Remove league average row and rank column
   return(stats)
 }
