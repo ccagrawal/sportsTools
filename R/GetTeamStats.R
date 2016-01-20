de3f5ec021340506c9f9b252dd536415ef678902
ccagrawal <ccagrawal@gmail.com>
2016-01-20 09:36:21 -0600

    Get more team stats

--- R/GetTeamStats.R
+++ R/GetTeamStats.R
@@ -10,7 +10,7 @@
 #' @examples
 #' GetTeamStats(2014)
 
-GetTeamStats <- function(year, stat.type = 'regular', season.type = 'regular') {
+GetTeamStats <- function(year, stat.type = 'regular', season.type = 'regular', keep.average = FALSE) {
   
   options(stringsAsFactors = FALSE)
   
@@ -25,16 +25,62 @@ GetTeamStats <- function(year, stat.type = 'regular', season.type = 'regular') {
   
   # Get table, convert appropriate columns to numeric, remove asterisks from team names
   if (stat.type == 'advanced') {
+    
     stats <- tables[['misc']]
     stats[, -c(2, 23, 24)] <- lapply(stats[, -c(2, 23, 24)], as.numeric)
     stats$Attendance <- as.numeric(gsub(',', '', stats$Attendance))
     stats$Team <- gsub('\\*', '', stats$Team)
-  } else {
+    
+  } else if (stat.type == 'team') {
+    
     stats <- tables[['team']]
     stats[, -2] <- lapply(stats[, -2], as.numeric)
     stats$Team <- gsub('\\*', '', stats$Team)
+    
+  } else if (stat.type == 'opponent') {
+    
+    stats <- tables[['opponent']]
+    stats[, -2] <- lapply(stats[, -2], as.numeric)
+    stats$Team <- gsub('\\*', '', stats$Team)
+    
+  } else if (stat.type == 'shooting') {
+    
+    stats <- tables[['shooting']]
+    stats[, -2] <- lapply(stats[, -2], as.numeric)
+    stats$Team <- gsub('\\*', '', stats$Team)
+    
+  } else if (stat.type == 'shooting opponent') {
+    
+    stats <- tables[['shooting_opp']]
+    stats[, -2] <- lapply(stats[, -2], as.numeric)
+    stats$Team <- gsub('\\*', '', stats$Team)
+    
+  } else if (stat.type == 'standings') {
+    
+    indices <- c(which(names(tables) == 'E_standings')[1], which(names(tables) == 'W_standings')[1])
+    temp.east <- tables[[indices[1]]]
+    temp.west <- tables[[indices[2]]]
+    colnames(temp.east)[1] <- 'Team'
+    colnames(temp.west)[1] <- 'Team'
+    
+    stats <- rbind(temp.east, temp.west)
+    stats[, -1] <- lapply(stats[, -1], as.numeric)
+    stats <- stats[-which(is.na(stats$W)), ]
+    
+    stats[which(is.na(stats$GB)), 'GB'] <- 0
+    colnames(stats)[1] <- 'Team'
+    stats$Seed <- as.numeric(gsub('[^(]*\\(([^)]*)\\)', '\\1', stats$Team))
+    
+    stats$Team <- gsub('[\\*|\\(].*', '', stats$Team)
+    stats$Team <- gsub('[^A-Za-z]$', '', stats$Team)
+    
   }
   
-  stats <- stats[-which(is.na(stats$Rk)), -1]   # Remove league average row and rank column
+  if (!keep.average) {
+    stats <- stats[-which(is.na(stats$Rk)), -1]   # Remove league average row and rank column
+  } else {
+    stats <- stats[, -1]
+  }
+  
   return(stats)
 }
