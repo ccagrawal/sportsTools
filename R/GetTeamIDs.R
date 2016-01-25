0acdde4d3baed0fd0d67c0009b47c09189b09696
ccagrawal <ccagrawal@gmail.com>
2016-01-25 12:20:25 -0600

    Update getting ESPN Team IDs

--- R/GetTeamIDs.R
+++ R/GetTeamIDs.R
@@ -22,6 +22,7 @@ GetTeamIDs <- function(year = 2016, source = 'NBA') {
     team.list <- data.frame(matrix(unlist(team.list), nrow = length(team.list), byrow = TRUE)) # Turn list to data frame
     
     team.list <- team.list[, c(1, 6)]      # Drop useless columns
+    colnames(team.list) <- c('id', 'name')
   } else if (source == 'ESPN') {
     url <- 'http://espn.go.com/nba/teams'
     lines <- readLines(url)
@@ -30,8 +31,11 @@ GetTeamIDs <- function(year = 2016, source = 'NBA') {
     team.list <- gsub('.*team/_/name/([^/]*)/([^\"]*).*', '\\1~\\2', lines)
     team.list <- strsplit(team.list, '~')
     team.list <- data.frame(matrix(unlist(team.list), nrow = length(team.list), byrow = TRUE))
+    colnames(team.list) <- c('id', 'name')
+    
+    team.list$name <- gsub('-', ' ', team.list$name)
+    team.list$name <- sapply(team.list$name, .CapLetters)
   }
   
-  colnames(team.list) <- c('id', 'name')
   return(team.list)
 }
