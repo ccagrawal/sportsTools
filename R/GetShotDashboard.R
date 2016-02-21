ce383f1d4dfb463e073b364d12690681445c9354
ccagrawal <ccagrawal@gmail.com>
2016-02-20 18:37:54 -0600

    Get player's shot dashboard

--- R/GetShotDashboard.R
+++ R/GetShotDashboard.R
@@ -3,12 +3,13 @@
 #' @param stat statistic to pull (e.g. 'Defender Distance')
 #' @param type either 'player' or 'team'
 #' @param id either player's ID or team's ID
+#' @param opponent.id opponent team's ID
 #' @return data frame of stats
 #' @keywords shooting player team
 #' @importFrom rjson fromJSON
 #' @export
 #' @examples
-#' GetShotDashboard('Defender Distance', 'team')
+#' GetShotDashboard('Defender Distance', 'player', '201147')
 
 GetShotDashboard <- function(stat, type = 'team', id, opponent.id = '0') {
   
@@ -55,6 +56,47 @@ GetShotDashboard <- function(stat, type = 'team', id, opponent.id = '0') {
     
     if (stat == 'Defender Distance') {
       char.cols <- c('TEAM_ID', 'TEAM_NAME', 'CLOSE_DEF_DIST_RANGE')
+      char.cols <- which(colnames(stats) %in% char.cols)
+      stats[, -char.cols] <- sapply(stats[, -char.cols], as.numeric)
+    }
+  } else {
+    url <- paste0('http://stats.nba.com/stats/playerdashptshots?',
+                  'DateFrom=&',
+                  'DateTo=&', 
+                  'GameSegment=&', 
+                  'LastNGames=0&', 
+                  'LeagueID=00&', 
+                  'Location=&', 
+                  'Month=0&', 
+                  'OpponentTeamID=', opponent.id, '&', 
+                  'Outcome=&',
+                  'PerMode=Totals&',
+                  'Period=0&',
+                  'PlayerID=', id, '&',
+                  'Season=2015-16&',
+                  'SeasonSegment=&',
+                  'SeasonType=Regular+Season&',
+                  'TeamID=0&',
+                  'VsConference=&',
+                  'VsDivision=')
+    
+    json <- fromJSON(file = url)[[3]]
+    
+    if (stat == 'Defender Distance') {
+      json <- json[[5]]
+    }
+    
+    stats <- json$rowSet
+    
+    # Create raw data frame
+    stats <- lapply(stats, lapply, function(x) ifelse(is.null(x), NA, x))   # Convert nulls to NAs
+    stats <- data.frame(matrix(unlist(stats), nrow = length(stats), byrow = TRUE)) # Turn list to data frame
+    
+    # Get column headers
+    colnames(stats) <- json$headers
+    
+    if (stat == 'Defender Distance') {
+      char.cols <- c('PLAYER_ID', 'PLAYER_NAME', 'CLOSE_DEF_DIST_RANGE')
       char.cols <- which(colnames(stats) %in% char.cols)
       stats[, -char.cols] <- sapply(stats[, -char.cols], as.numeric)
     }
