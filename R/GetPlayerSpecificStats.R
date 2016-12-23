03737bb2fee8756c70f2b4f87755c25596022bf8
Chirag Agrawal <ccagrawal@gmail.com>
2016-12-22 23:44:24 -0600

    Fix single row player stats

--- R/GetPlayerSpecificStats.R
+++ R/GetPlayerSpecificStats.R
@@ -6,22 +6,19 @@
 #' @param year NBA season (e.g. 2008 for the 2007-08 season)
 #' @param season.type Either 'Regular Season' or 'Playoffs'
 #' @param player.ids Players and their IDs
-#' @param source Either 'Basketball-Reference' or 'NBA'
 #' @return data frame with player's stats
 #' @keywords player
-#' @importFrom XML readHTMLTable
 #' @importFrom httr GET content add_headers
 #' @export
 #' @examples
-#' GetPlayerSpecificStats('Anthony Davis', 'on-off', 2015)
+#' GetPlayerSpecificStats('Anthony Davis', 'on-Scoring', 2015)
 
 GetPlayerSpecificStats <- function(player, 
                                    measure.type,
                                    per.mode = 'Totals',
                                    year = CurrentYear(),
                                    season.type = 'Regular Season',
-                                   player.ids,
-                                   source = 'NBA') {
+                                   player.ids) {
   
   options(stringsAsFactors = FALSE)
   
@@ -32,70 +29,6 @@ GetPlayerSpecificStats <- function(player,
     }
     player <- player.ids[which(player.ids$DISPLAY_FIRST_LAST == player), 'PERSON_ID']
   }
-  
-  if (source == 'Basketball-Reference') {
-    return(.GetPlayerSpecificStatsBRef(player, measure.type, year))
-  } else if (source == 'NBA') {
-    return(.GetPlayerSpecificStatsNBA(player, measure.type, per.mode, season.type, year))
-  }
-}
-
-.GetPlayerSpecificStatsBRef <- function(player, measure.type, year) {
-
-  base.url <- paste0('http://www.basketball-reference.com/players', player)
-  
-  if (measure.type == 'on-off') {
-    url <- paste0(base.url, '/on-off/', year, '/')
-    
-    table <- readHTMLTable(url)[[1]]
-    table[, -c(1, 2)] <- sapply(table[, -c(1, 2)], as.numeric)
-  } else if (measure.type == 'per game') {
-    url <- paste0(base.url, '.html')
-    
-    table <- readHTMLTable(url)[['per_game']]
-    table[, -c(1, 3, 4, 5)] <- sapply(table[, -c(1, 3, 4, 5)], as.numeric)
-    table$Season <- as.numeric(substr(table$Season, 1, 4)) + 1
-  } else if (measure.type == 'totals') {
-    url <- paste0(base.url, '.html')
-    
-    table <- readHTMLTable(url)[['totals']]
-    table[, -c(1, 3, 4, 5)] <- sapply(table[, -c(1, 3, 4, 5)], as.numeric)
-    table$Season <- as.numeric(substr(table$Season, 1, 4)) + 1
-  } else if (measure.type == 'shooting') {
-    url <- paste0(base.url, '/shooting/', year, '/')
-    
-    table <- readHTMLTable(url)[[1]]
-    table <- table[3:nrow(table), ]
-    colnames(table) <- table[1, ]
-    table <- table[-which(table$Split == 'Split'), ]
-    table[, 3:11] <- sapply(table[, 3:11], as.numeric)
-  } else if (measure.type == 'play by play') {
-    url <- paste0(base.url, '.html')
-    
-    table <- readHTMLTable(url)[['advanced_pbp']]
-    table[, c(8:12)] <- sapply(table[, c(8:12)], function(x) gsub('%', '', x))
-    table[, -c(1, 3, 4, 5)] <- sapply(table[, -c(1, 3, 4, 5)], as.numeric)
-    table[, c(8:12)] <- table[, c(8:12)] / 100
-    table$Season <- as.numeric(substr(table$Season, 1, 4)) + 1
-  } else if (measure.type == 'advanced') {
-    url <- paste0(base.url, '.html')
-    
-    table <- readHTMLTable(url)[['advanced']]
-    table <- table[, -c(20, 25)]
-    table[, -c(1, 3, 4, 5)] <- sapply(table[, -c(1, 3, 4, 5)], as.numeric)
-    table$Season <- as.numeric(substr(table$Season, 1, 4)) + 1
-  } else if (measure.type == 'playoff totals') {
-    url <- paste0(base.url, '.html')
-    
-    table <- readHTMLTable(url)[['playoffs_totals']]
-    table[, -c(1, 3, 4, 5)] <- sapply(table[, -c(1, 3, 4, 5)], as.numeric)
-    table$Season <- as.numeric(substr(table$Season, 1, 4)) + 1
-  }
-  
-  return(table)
-}
-
-.GetPlayerSpecificStatsNBA <- function(player, measure.type, per.mode, season.type, year) {
   
   request <- GET(
     "http://stats.nba.com/stats/playerdashboardbyyearoveryear",
@@ -136,7 +69,9 @@ GetPlayerSpecificStats <- function(player,
   stats <- stats[, -remove.cols]
   
   stats$GROUP_VALUE <- SeasonToYear(stats$GROUP_VALUE)
-  stats <- data.frame(sapply(stats, as.numeric))
+  char.cols <- c('TEAM_ID', 'GROUP_VALUE')
+  char.cols <- which(colnames(stats) %in% char.cols)
+  stats[, -char.cols] <- sapply(stats[, -char.cols], as.numeric)
   
   return(stats)
 }
