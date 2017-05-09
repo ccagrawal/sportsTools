1a708c696ed1514fd82c7d8913c0c13363b3e4c2
Chirag Agrawal <ccagrawal@gmail.com>
2017-05-08 23:55:19 -0500

    Fix player specific stats

--- R/GetPlayerSpecificStats.R
+++ R/GetPlayerSpecificStats.R
@@ -11,7 +11,7 @@
 #' @importFrom httr GET content add_headers
 #' @export
 #' @examples
-#' GetPlayerSpecificStats('Anthony Davis', 'on-Scoring', 2015)
+#' GetPlayerSpecificStats('Anthony Davis', 'Scoring', 2015)
 
 GetPlayerSpecificStats <- function(player, 
                                    measure.type,
@@ -54,7 +54,11 @@ GetPlayerSpecificStats <- function(player,
       VsConference = "",
       VsDivision = ""
     ),
-    add_headers('User-Agent' = 'Mozilla/5.0')
+    add_headers(
+      'Accept-Language' = 'en-US,en;q=0.8,af;q=0.6',
+      'Referer' = 'http://stats.nba.com/player/',
+      'User-Agent' = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+    )
   )
   
   content <- content(request, 'parsed')[[3]][[2]]
