748a6e88a4cf5922496668bcb1b9bfa757e96b24
Chirag Agrawal <ccagrawal@gmail.com>
2017-05-10 20:05:09 -0500

    Fix schedule headers

--- R/GetSchedule.R
+++ R/GetSchedule.R
@@ -26,7 +26,10 @@ GetSchedule <- function(year = CurrentYear(), season.type = 'Regular Season') {
       SeasonType = season.type,
       Sorter = "DATE"
     ),
-    add_headers('User-Agent' = 'Mozilla/5.0')
+    add_headers(
+      'Accept-Language' = 'en-US,en;q=0.8,af;q=0.6',
+      'User-Agent' = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+    )
   )
   
   content <- content(request, 'parsed')[[3]][[1]]
